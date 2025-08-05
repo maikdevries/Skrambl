1ac660f09d87bd87dc46c65da8bf2aac82e3eb2b
maikdevries <dev.maik.devries@outlook.com>
2025-08-05 18:10:38 +0200

    refactor: replace routing module with middleware-aware module

--- app.ts
+++ app.ts
@@ -2,4 +2,4 @@ import router from './src/server/router.ts';
 
 Deno.serve({
 	'port': Number.parseInt(Deno.env.get('DENO_PORT') ?? ''),
-}, async (request: Request) => await router(request));
+}, async (request: Request) => await router(request, {}));
--- src/server/router.ts
+++ src/server/router.ts
@@ -1,47 +1,42 @@
-import type { Route } from '@std/http/unstable-route';
-
+import { route } from '@maikdevries/server-router';
 import { session } from '@maikdevries/server-sessions';
 import { STATUS_CODE, STATUS_TEXT } from '@std/http';
-import { route } from '@std/http/unstable-route';
 
 import * as auth from './controllers/auth.controllers.ts';
 import * as spotify from './controllers/spotify.controllers.ts';
 
-const routes: Route[] = [
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/' }),
-		'handler': (request: Request) => new Response(`Welcome to ${URL.parse(request.url)?.hostname}`),
-	},
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/auth/login' }),
-		'handler': (request: Request) => session.handle(request, auth.login),
-	},
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/auth/process' }),
-		'handler': (request: Request) => session.handle(request, auth.process),
-	},
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/auth/refresh' }),
-		'handler': (request: Request) => session.handle(request, auth.refresh),
-	},
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/auth/logout' }),
-		'handler': (request: Request) => session.handle(request, auth.logout),
-	},
-	{
-		'method': ['GET'],
-		'pattern': new URLPattern({ 'pathname': '/playlists' }),
-		'handler': (request: Request) => session.handle(request, spotify.playlists),
-	},
-];
-
-function defaultHandler(_: Request): Response {
-	return new Response(STATUS_TEXT[STATUS_CODE.NotFound], { 'status': STATUS_CODE.NotFound });
-}
-
-export default route(routes, defaultHandler);
+export default route(
+	[
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/' }),
+			'handler': (_, context) => new Response(`Welcome to ${context.url.hostname}`),
+		},
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/auth/login' }),
+			'handler': (request) => session.handle(request, auth.login),
+		},
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/auth/process' }),
+			'handler': (request) => session.handle(request, auth.process),
+		},
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/auth/refresh' }),
+			'handler': (request) => session.handle(request, auth.refresh),
+		},
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/auth/logout' }),
+			'handler': (request) => session.handle(request, auth.logout),
+		},
+		{
+			'method': ['GET'],
+			'pattern': new URLPattern({ 'pathname': '/playlists' }),
+			'handler': (request) => session.handle(request, spotify.playlists),
+		},
+	],
+	() => new Response(STATUS_TEXT[STATUS_CODE.NotFound], { 'status': STATUS_CODE.NotFound }),
+);
