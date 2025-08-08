9a04f6d89fa5555eba203868af1083a1d92299c4
maikdevries <dev.maik.devries@outlook.com>
2025-08-08 16:23:57 +0200

    feat: define HTML base template

---
+++ src/server/templates/base.templates.ts
@@ -0,0 +1,25 @@
+import { render } from '@maikdevries/server-render';
+
+export const Base = render.html`
+	<!DOCTYPE html>
+	<html lang='en'>
+		<head>
+			<meta charset='UTF-8'>
+			<meta name='viewport' content='width = device-width, initial-scale = 1.0'>
+		</head>
+
+		<body>
+			<header>
+				<!-- [TODO] Define header template -->
+			</header>
+
+			<main>
+				<!-- [TODO] Support insertion of main content -->
+			</main>
+
+			<footer>
+				<!-- [TODO] Define footer template -->
+			</footer>
+		</body>
+	</html>
+`;
