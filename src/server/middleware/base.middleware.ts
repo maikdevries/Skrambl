488feab105954a00e17d794ab1d29731df2a07b9
maikdevries <dev.maik.devries@outlook.com>
2025-08-19 16:39:50 +0200

    feat: implement naive error handler middleware

--- src/server/middleware/base.middleware.ts
+++ src/server/middleware/base.middleware.ts
@@ -1,4 +1,15 @@
+import type { Middleware } from '@maikdevries/server-router';
+
 import { chain } from '@maikdevries/server-router';
 import { session } from '@maikdevries/server-sessions';
 
-export default chain(session());
+const error: Middleware = async (request, context, next) => {
+	try {
+		return await next(request, context);
+	} catch (error) {
+		console.error(error);
+		return new Response('Internal Server Error', { 'status': 500 });
+	}
+};
+
+export default chain(error).add(session());
