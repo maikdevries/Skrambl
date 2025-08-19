e2a711fc591bd431d2cac31b743babe4eb0d06f7
maikdevries <dev.maik.devries@outlook.com>
2025-08-19 18:26:35 +0200

    feat: include base CSS stylesheet in base template

--- src/server/templates/base.templates.ts
+++ src/server/templates/base.templates.ts
@@ -8,6 +8,8 @@ export const Base = ((content: Template) => html`
 		<head>
 			<meta charset='UTF-8'>
 			<meta name='viewport' content='width = device-width, initial-scale = 1.0'>
+
+			<link rel='stylesheet' href='/css/base.css'>
 		</head>
 
 		<body>
