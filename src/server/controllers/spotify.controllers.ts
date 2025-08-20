11050398c57f1bdeb4e6b72d21ec937b194592a2
maikdevries <dev.maik.devries@outlook.com>
2025-08-20 14:38:59 +0200

    refactor: rename base routes router to public routes

--- src/server/controllers/spotify.controllers.ts
+++ src/server/controllers/spotify.controllers.ts
@@ -1,5 +1,5 @@
 import type { Credentials } from '../common/types.ts';
-import type { Context } from '../routes/base.routes.ts';
+import type { Context } from '../routes/public.routes.ts';
 
 import * as spotify from '../services/spotify.services.ts';
 
