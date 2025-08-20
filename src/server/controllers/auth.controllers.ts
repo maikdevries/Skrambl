76b4b4b95c1f7a100d32c85ef2c468907d538f31
maikdevries <dev.maik.devries@outlook.com>
2025-08-20 14:34:31 +0200

    fix: update authorisation prompt routing path

--- src/server/controllers/auth.controllers.ts
+++ src/server/controllers/auth.controllers.ts
@@ -42,7 +42,7 @@ export async function process(_: Request, context: Context): Promise<Response> {
 	const code = context.url.searchParams.get('code');
 	const pkce = context.session.get<PKCE>('pkce');
 
-	if (!code || !pkce) return Response.redirect(new URL('/auth', DENO_ORIGIN));
+	if (!code || !pkce) return Response.redirect(new URL('/connect', DENO_ORIGIN));
 	else if (context.url.searchParams.get('state') !== pkce.state) return Response.redirect(new URL('/auth/csrf', DENO_ORIGIN));
 
 	const credentials = await auth.retrieve(code, pkce.verifier);
@@ -53,7 +53,7 @@ export async function process(_: Request, context: Context): Promise<Response> {
 
 export async function refresh(_: Request, context: Context): Promise<Response> {
 	const token = context.session.get<Credentials>('credentials')?.refresh;
-	if (!token) return Response.redirect(new URL('/auth', DENO_ORIGIN));
+	if (!token) return Response.redirect(new URL('/connect', DENO_ORIGIN));
 
 	const credentials = await auth.refresh(token);
 	context.session.regenerate().set('credentials', credentials);
