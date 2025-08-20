315fc70474be3dcbf937ef629607a67a6a7f0c9e
maikdevries <dev.maik.devries@outlook.com>
2025-08-20 12:06:26 +0200

    feat: define authorisation page template

--- src/server/templates/pages.templates.ts
+++ src/server/templates/pages.templates.ts
@@ -5,6 +5,20 @@ import { html } from '@maikdevries/server-render';
 import { Base } from './base.templates.ts';
 import { ListComponent, PlaylistComponent } from './components.templates.ts';
 
+export const Authorisation = (() => Base(html`
+	<section>
+		${
+			ListComponent('Plug into a streaming service', [
+				html`<a href='/auth/login'>Connect with Spotify</a>`,
+			])
+		}
+
+		<footer>
+			<strong>Don't worry</strong>, none of your private information is ever shared with us.
+		</footer>
+	</section>
+`));
+
 export const Dashboard = ((items: Playlist[]) => Base(html`
 	<section>
 		${ ListComponent('Playlists', items.map(PlaylistComponent)) }
