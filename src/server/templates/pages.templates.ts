13650ddedcabd3525191c4a82776917a377c6595
maikdevries <dev.maik.devries@outlook.com>
2025-08-20 10:58:35 +0200

    refactor: rename home page to dashboard

--- src/server/templates/pages.templates.ts
+++ src/server/templates/pages.templates.ts
@@ -5,7 +5,7 @@ import { html } from '@maikdevries/server-render';
 import { Base } from './base.templates.ts';
 import { ListComponent, PlaylistComponent } from './components.templates.ts';
 
-export const Home = ((items: Playlist[]) => Base(html`
+export const Dashboard = ((items: Playlist[]) => Base(html`
 	<section>
 		${ ListComponent('Playlists', items.map(PlaylistComponent)) }
 	</section>
