e1fa0083d4f368c7e358fde1934ce5f09d6eef08
maikdevries <dev.maik.devries@outlook.com>
2025-08-27 16:54:21 +0200

    feat: retrieve playlists to be used in tool template

--- src/server/controllers/tool.controllers.ts
+++ src/server/controllers/tool.controllers.ts
@@ -1,10 +1,14 @@
 import type { Context } from '../routes/tool.routes.ts';
 
 import { stringify as render } from '@maikdevries/server-render';
+
+import * as spotify from '../services/spotify.services.ts';
 import * as templates from '../templates/pages.templates.ts';
 
-export async function base(_: Request, __: Context): Promise<Response> {
-	return new Response(await render(templates.Tool([])), {
+export async function base(_: Request, context: Context): Promise<Response> {
+	const playlists = await spotify.getPlaylists(context.credentials.token);
+
+	return new Response(await render(templates.Tool(playlists)), {
 		'headers': {
 			'Content-Type': 'text/html; charset=utf-8',
 		},
