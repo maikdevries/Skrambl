baccaa6da28db5ca79001c86ab84056a7c379f28
maikdevries <dev.maik.devries@outlook.com>
2025-08-30 11:18:43 +0200

    feat: implement request logger base middleware

--- src/server/middleware/base.middleware.ts
+++ src/server/middleware/base.middleware.ts
@@ -1,3 +1,4 @@
+import type { Middleware } from '@maikdevries/server-router';
 import { chain } from '@maikdevries/server-router';
 
 import type { Session } from '@maikdevries/server-sessions';
@@ -7,4 +8,13 @@ export interface BaseContext {
 	'session': Session;
 }
 
-export default chain(session());
+const logger: Middleware = async (request, context, next) => {
+	const start = performance.now();
+	const response = await next(request, context);
+	const end = performance.now();
+
+	console.log(`[${(end - start).toFixed(2)} ms] ${request.method} ${request.url} - ${response.status}`);
+	return response;
+}
+
+export default chain(logger).add(session());
