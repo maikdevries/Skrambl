9afeaf1146a8b36e62c192f4b97f0232fa8fa5d2
maikdevries <dev.maik.devries@outlook.com>
2025-08-30 11:34:16 +0200

    refactor: prefix static client content routes with static

--- src/server/templates/base.templates.ts
+++ src/server/templates/base.templates.ts
@@ -10,7 +10,7 @@ export const Base = ((content: Template) => html`
 			<meta charset='UTF-8'>
 			<meta name='viewport' content='width = device-width, initial-scale = 1.0'>
 
-			<link rel='stylesheet' href='/css/base.css'>
+			<link rel='stylesheet' href='/static/css/base.css'>
 		</head>
 
 		<body>
