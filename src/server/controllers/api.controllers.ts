55c73e4bc2471386dbc081c8a15cbd256f0f5831
maikdevries <dev.maik.devries@outlook.com>
2025-09-06 17:58:40 +0200

    feat: implement api routes and controller

---
+++ src/server/controllers/api.controllers.ts
@@ -0,0 +1,36 @@
+import type { Context } from '../routes/api.routes.ts';
+
+import * as spotify from '../services/spotify.services.ts';
+
+const OPERATIONS = {
+	'shuffle': shuffle,
+};
+
+export async function process(request: Request, context: Context): Promise<Response> {
+	// [TODO] Proper validation whether request body is JSON and has 'operation' (string) and 'items' (string[]) properties
+	const { operation, items } = await request.json() as { 'operation': keyof typeof OPERATIONS; 'items': string[] };
+
+	const promises = items.map(async (id) => {
+		const tracks = await spotify.getPlaylistItems(context.credentials.token, id);
+		if (!tracks.length) return;
+
+		await spotify.removePlaylistItems(context.credentials.token, id, tracks);
+		await spotify.addPlaylistItems(context.credentials.token, id, OPERATIONS[operation](tracks));
+	});
+
+	const results = await Promise.allSettled(promises);
+
+	if (results.every((x) => x.status === 'fulfilled')) {
+		return Response.json({ 'description': 'The operation has been processed successfully' });
+	} else return Response.json({ 'description': 'Something went terribly wrong on our side of the internet' }, { 'status': 500 });
+}
+
+// [NOTE] Durstenfeld variant of the Fisher-Yates shuffle algorithm
+function shuffle<T>(array: T[]): T[] {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]] as [T, T];
+	}
+
+	return array;
+}
--- src/server/routes/api.routes.ts
+++ src/server/routes/api.routes.ts
@@ -4,6 +4,8 @@ import { route } from '@maikdevries/server-router';
 import type { BaseContext } from '../middleware/api.middleware.ts';
 import middleware from '../middleware/api.middleware.ts';
 
+import * as api from '../controllers/api.controllers.ts';
+
 export type Context = RouteContext<BaseContext>;
 
 const router = route<BaseContext>(
@@ -12,6 +14,11 @@ const router = route<BaseContext>(
 			'method': ['GET'],
 			'pattern': new URLPattern({ 'pathname': '/api/status' }),
 			'handler': () => Response.json({ 'status': 'OK' }),
+		},
+		{
+			'method': ['POST'],
+			'pattern': new URLPattern({ 'pathname': '/api/tool/process' }),
+			'handler': api.process,
 		},
 	],
 	() => Response.json({ 'description': 'The requested resource could not be found' }, { 'status': 404 }),
