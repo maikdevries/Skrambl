8291c1e4692e7576737931f7f796e6b372c264e1
maikdevries <dev.maik.devries@outlook.com>
2025-09-08 14:42:00 +0200

    feat: configure event listeners to be passive

--- src/client/ts/tool.ts
+++ src/client/ts/tool.ts
@@ -12,6 +12,9 @@ document.addEventListener(
 		if (event.detail['operation'] === 'ADD') return queue.append(element);
 		else if (event.detail['operation'] === 'REMOVE') return playlists.append(element);
 	}) as EventListener,
+	{
+		'passive': true,
+	},
 );
 
 const [stop, play] = Array.from(document.querySelectorAll<HTMLButtonElement>('main > aside > footer > button'));
@@ -20,29 +23,35 @@ if (!stop || !play) throw new Error();
 // [TODO] Support for aborting ongoing operation
 // stop.addEventListener('click', async (event: Event) => {});
 
-play.addEventListener('click', async (event: Event) => {
-	const button = event.target instanceof HTMLButtonElement ? event.target : null;
-	if (!button) return;
+play.addEventListener(
+	'click',
+	async (event: Event) => {
+		const button = event.target instanceof HTMLButtonElement ? event.target : null;
+		if (!button) return;
 
-	button.disabled = true;
+		button.disabled = true;
 
-	try {
-		const items = Array.from(queue.querySelectorAll<PlaylistElement>('x-playlist')).map((x) => x.id);
+		try {
+			const items = Array.from(queue.querySelectorAll<PlaylistElement>('x-playlist')).map((x) => x.id);
 
-		await fetch(new URL('/api/tool/process', document.location.origin), {
-			'method': 'POST',
-			'headers': {
-				'Content-Type': 'application/json',
-			},
-			'body': JSON.stringify({
-				'operation': 'SHUFFLE',
-				'items': items,
-			}),
-		});
-	} catch (error: unknown) {
-		// [TODO] Implement proper general error handler
-		console.error(error);
-	}
+			await fetch(new URL('/api/tool/process', document.location.origin), {
+				'method': 'POST',
+				'headers': {
+					'Content-Type': 'application/json',
+				},
+				'body': JSON.stringify({
+					'operation': 'SHUFFLE',
+					'items': items,
+				}),
+			});
+		} catch (error: unknown) {
+			// [TODO] Implement proper general error handler
+			console.error(error);
+		}
 
-	button.disabled = false;
-});
+		button.disabled = false;
+	},
+	{
+		'passive': true,
+	},
+);
