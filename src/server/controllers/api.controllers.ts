090a6bb9df44d10ad7443870c4967384822837c7
maikdevries <dev.maik.devries@outlook.com>
2025-09-27 15:30:49 +0200

    feat: cache playlist items in case of error

--- src/server/controllers/api.controllers.ts
+++ src/server/controllers/api.controllers.ts
@@ -1,6 +1,7 @@
 import type { Context } from '../routes/api.routes.ts';
-import { ServerError } from '../types/base.types.ts';
+import type { PlaylistItem } from '../types/spotify.types.ts';
 
+import { ServerError } from '../types/base.types.ts';
 import * as spotify from '../services/spotify.services.ts';
 
 const OPERATIONS = {
@@ -10,16 +11,24 @@ const OPERATIONS = {
 export async function process(request: Request, context: Context): Promise<Response> {
 	// [TODO] Proper validation whether request body is JSON and has 'operation' (string) and 'items' (string[]) properties
 	const { operation, items } = await request.json() as { 'operation': keyof typeof OPERATIONS; 'items': string[] };
+	const cache: Map<string, PlaylistItem[]> = context.session.get('cache') ?? new Map();
 
 	const promises = items.map(async (id) => {
-		const tracks = await spotify.getPlaylistItems(context.credentials.token, id);
+		const tracks = cache.get(id) ?? await spotify.getPlaylistItems(context.credentials.token, id);
 		if (!tracks.length) return;
+
+		// [NOTE] Create cache entry for current playlist to avoid loss of data when error is thrown hereafter
+		if (!cache.has(id)) cache.set(id, tracks);
 
 		await spotify.removePlaylistItems(context.credentials.token, id, tracks);
 		await spotify.addPlaylistItems(context.credentials.token, id, OPERATIONS[operation](tracks));
+
+		// [NOTE] When successful, clear cache from current playlist
+		return cache.delete(id);
 	});
 
 	const results = await Promise.allSettled(promises);
+	if (!context.session.has('cache')) context.session.set('cache', cache);
 
 	if (results.every((x) => x.status === 'fulfilled')) {
 		return Response.json({ 'description': 'The operation has been processed successfully' });
