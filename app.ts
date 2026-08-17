20e0d5f1a3187ee970badef779786392ad142c7a
maikdevries <dev.maik.devries@outlook.com>
2026-08-17 16:30:54 +0200

    fix: enable automatic compression of responses

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import router from './src/server/routes/base.routes.ts';
 
 Deno.serve({
+	'automaticCompression': true,
+	'hostname': '0.0.0.0',
 	'port': Number.parseInt(Deno.env.get('DENO_PORT') ?? ''),
 }, (request: Request) => router(request, {}));
